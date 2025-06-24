3e27f74e30cf3c5185112567e532c8923f08031e
amrit <amritkumarverma848@gmail.com>
2025-06-24 16:40:06 +0530

    Components Added : NavBar added

---
+++ Frontend/IPO Web App/src/component/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+      <nav className='bg-black text-white px-6 py-4 flex justify-between items-center shadow-md'>
+        <Link to="/" className="text-2xl font-bold text-purple-500">IPO Platform</Link>
+      
+      <div className="space-x-6 text-sm">
+        <Link to="/" className="hover:text-purple-400">Home</Link>
+        <Link to="/admin" className="hover:text-purple-400">Admin</Link>
+      </div>
+      </nav>
+    </div>
+  )
+}
+
+export default NavBar
