3c5f3bb4229f30fd27d7975dc3bb0c9d51c2850a
amrit <amritkumarverma848@gmail.com>
2025-06-24 17:01:00 +0530

    Dummy Values added

---
+++ Frontend/IPO Web App/src/utils/constants.js
@@ -0,0 +1,57 @@
+export const dummyIPO = [
+    {
+        id : "1",
+        company : "Tata Technologies",
+        priceBand : '150 - 180',
+        lotSize : 15,
+        sector : "Tech",
+        openDate: "26-06-2025",
+        endDate : "01-07-2025",
+        rhp : "https://www.example.com/rhp-tata",
+        description : 'A tech company that build services based on requirements.'
+    },
+    {
+        id : "2",
+        company : "HDFC Banks",
+        priceBand : '950 - 1080',
+        lotSize : 15,
+        sector : "Bank",
+        openDate: "26-06-2025",
+        endDate : "01-07-2025",
+        rhp : "https://www.example.com/rhp-hdfc",
+        description : 'A highly trusted Bank that build services based on requirements.'
+    },
+    {
+    id: '3',
+    company: 'Sun Pharma',
+    priceBand: '850 - 900',
+    lotSize: 20,
+    sector: 'Pharma',
+    openDate: '2025-07-10',
+    closeDate: '2025-07-14',
+    rhp: 'https://example.com/rhp-sunpharma',
+    description: 'Top pharmaceutical and healthcare company in India.'
+  },
+  {
+    id: '4',
+    company: 'Maruti Motors',
+    priceBand: '1800 - 2000',
+    lotSize: 25,
+    sector: 'Automobile',
+    openDate: '2025-07-18',
+    closeDate: '2025-07-22',
+    rhp: 'https://example.com/rhp-maruti',
+    description: 'India’s largest car manufacturer with global presence.'
+  },
+  {
+    id: '5',
+    company: 'Reliance Power',
+    priceBand: '95 - 110',
+    lotSize: 100,
+    sector: 'Energy',
+    openDate: '2025-07-25',
+    closeDate: '2025-07-29',
+    rhp: 'https://example.com/rhp-reliancepower',
+    description: 'A leading player in the Indian power and energy sector.'
+  }
+]
