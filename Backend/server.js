9f547dd2af9dff277026786b1031b01b35c4ce3e
amrit <amritkumarverma848@gmail.com>
2025-06-28 21:23:15 +0530

    Necessary modules installed

--- Backend/server.js
+++ Backend/server.js
@@ -4,16 +4,19 @@ const connectDB = require( "./config/db")
 const cors = require( "cors")
 
 dotenv.config()
-connectDB();
+
 const app = express();
 
 
 app.use(cors());
 app.use(express.json());
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('IPO Backend API Running')
 })
+
+app.use('/api/auth', require('./routes/authRoutes'))
 
 const PORT = process.env.PORT || 5000
 
